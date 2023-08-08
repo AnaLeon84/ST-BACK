d85127973b46079b5f7691056909408d1d563ad7
AnaLeon84 <ana.cableon@hotmail.com>
2023-08-08 19:43:45 +0200

    installation sequelize and routes copied from the specification notebook to the router file

--- APP/router.js
+++ APP/router.js
@@ -8,6 +8,18 @@ router.get('/', mainController.home);
 router.get('/login', mainController.login);
 router.get('/story/:id', storyController.story);
 
+// GET /stories : Récupérer toutes les histoires publiques 
+// GET /stories/:id : Récupérer une histoire spécifique 
+//POST /stories : Créer une nouvelle histoire 
+//PUT /stories/:id : Mettre à jour une histoire existante 
+//DELETE /stories/:id : Supprimer une histoire 
+//GET /user/:id/my-story : Récupérer toutes les histoires de l'utilisateur connecté 
+//POST /user/:id/favorite : Ajouter une histoire à la liste de favoris de l'utilisateur connecté 
+//GET /user/:id/favorite : Récupérer la liste des histoires favorites de l'utilisateur connecté 
+//DELETE /user/:id/favorite/:id : Supprimer une histoire de la liste de favoris de l'utilisateur connecté 
+//PUT /user/:id/profil : Mettre à jour le compte de l'utilisateur connecté (pseudo et mot de passe) 
+
+
 router.use(mainController.notFound);
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 
+const session = require('express-session');
 const app = express();
 
 const router = require('./app/router');
